f8b3142c76d2951a0400ea5e4c1f4680078961b6
haxzie <musthu.gm@gmail.com>
2018-10-27 14:42:03 +0530

    Updated swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() 
+{
+	int x = 10;
+	int y = 5;
+
+	printf("\n Values before swapping");
+	printf("\n X: %d\n Y: %d\n", x, y);
+
+	// add swap logic here
+	
+	printf("\n Values after swapping");
+	printf("\n X: %d\n Y: %d\n", x, y);
+}
